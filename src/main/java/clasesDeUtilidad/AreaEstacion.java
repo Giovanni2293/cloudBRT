9f725d7b9d80cd7e73b5269c8b9c624193abe193
andynight <carper010203@hotmail.com>
2016-08-30 21:57:32 -0500

    Delegado el metodo calcularDistancia a GeoMatematicas

--- src/main/java/clasesDeUtilidad/AreaEstacion.java
+++ src/main/java/clasesDeUtilidad/AreaEstacion.java
@@ -1,7 +1,7 @@
 package clasesDeUtilidad;
 
 import clasesDelBRT.Coordenadas;
-import java.lang.Math;
+
 
 public class AreaEstacion {
 
@@ -27,36 +27,12 @@ public class AreaEstacion {
 	{
 		return distancia;
 	}
-	
-	public double calcDistancia(Coordenadas vertice,Coordenadas bus)
-	{
-		double lat1=0,lng1=0;
-		double lat2=0,lng2=0;
-		
-		lat1 = vertice.getLatitud();
-		lng1 = vertice.getLongitud();
-		lat2 = bus.getLatitud();
-		lng2 = bus.getLongitud();		
-		
-		double radioTierra = 6371;// en kilómetros
-		double dLat,dLng,sindLat,sindLng,va1,va2;
-		dLat = Math.toRadians(lat2 - lat1);
-		dLng = Math.toRadians(lng2 - lng1);
-		sindLat = Math.sin(dLat / 2);
-		sindLng = Math.sin(dLng / 2);
-		va1 = Math.pow(sindLat, 2)
-				+ Math.pow(sindLng, 2) * Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2));
-		va2 = 2 * Math.atan2(Math.sqrt(va1), Math.sqrt(1 - va1));
-		double distancia = radioTierra * va2;
-		this.distancia=distancia*1000;
-		return this.distancia;
-		
-	}
-	
+			
 	public boolean estaDentro(Coordenadas vertice,Coordenadas bus)
 	{
 		double temp;
-		temp = calcDistancia(vertice,bus);
+		temp = GeoMatematicas.calcDistancia(vertice,bus);
+		this.distancia = temp;
 		if (temp>= radio)
 		{
 			return false;
